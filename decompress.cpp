b8c9752a4471b746bc8cc30103157ebb396c22ee
pratyu-J <48614443+pratyu-J@users.noreply.github.com>
2019-12-01 03:41:00 +0530

    decompress
    user gives the zip name to open

--- decompress.cpp
+++ decompress.cpp
@@ -23,27 +23,30 @@ void decompress()
 {
 int cnt=1;
 ifstream fin;
+string arcname;
+cout<<"enter the name of zip file"<<endl;
+cin>>arcname;
 char ch='y';
 while(1)
     {
         if(ch=='y')
         {
         string val = "file"+to_string(cnt)+".txt";
-        system(("7za e archive.zip archive"+to_string(cnt)+".zip").c_str());
-        string file = "7za e archive"+to_string(cnt)+".zip";
+        system(("7za e " + arcname + ".zip " + arcname +to_string(cnt)+".zip").c_str());
+      string file = "7za e " + arcname+to_string(cnt)+".zip";
     const char* filecmd = file.c_str();
     system(filecmd);
         cout<<"printing "<<val<<endl;
         cnt++;
-//     fin.open(val);
-//     string line;
-//     cout<<val<<endl;
+     fin.open(val);
+     string line;
+     cout<<val<<endl;
 //     while(fin)
 //         {
 //             getline(fin,line);
 //             cout<<line<<endl;
 //         }
-//        //  nfile--;
+//       //  nfile--;
 //          fin.close();
     }
     else
@@ -57,7 +60,7 @@ while(1)
         char fname1[valu.length()+1];
         strcpy(fname1, valu.c_str());
          deleteFile(fname1);
-         string fileN="archive"+(to_string(cnt2))+".zip";
+         string fileN=arcname+(to_string(cnt2))+".zip";
         fname1[fileN.length()+1];
         strcpy(fname1, fileN.c_str());
          deleteFile(fname1);
